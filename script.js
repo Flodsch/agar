04b5d397e1010a14a7c246ad90e03ede3227ac7f
Florian Hink <florian.hink@gmail.com>
2015-08-06 22:49:14 +0200

    added player class

--- script.js
+++ script.js
@@ -9,6 +9,10 @@ var Class = function() {
 // TODO: Anziehungskraft!
 // TODO: mache Einsaugen nicht direkt bei Kollision
 // TODO: fix canvas size
+// TODO: Vererbung für Player Class fixen -> remove redundant code
+// TODO: set max velocity
+// TODO: add winning condition
+// TODO: add losing condition
 
 // Stage class
 var Stage = Class();
@@ -109,7 +113,7 @@ Blob.prototype = {
     update: function() {
         this.x += this.vX;
         this.y += this.vY;
-        this.radius = this.mass * this.config.blobsize;
+        this.radius = this.mass * 0.6;
         //console.log(this.radius);
     },
     checkBoundaryCollision: function() {
@@ -137,10 +141,85 @@ Blob.prototype = {
 var Config = Class();
 Config.prototype = {
   constructor: function() {
-    this.enemies = 200;
+    this.enemies = 20;
     this.difficulty = "easy";
     this.fieldsize = 1000;
     this.blobsize = 0.6;
+  }
+}
+
+var Player = Class();
+Player.prototype = {
+  constructor: function(x, y, mass, color, vX, vY, config) {
+      this.radius = mass * 0.6;
+      this.mass = mass;
+      this.color = color;
+      this.stage = null ;
+      this.x = x;
+      this.y = y;
+      this.vX = vX;
+      this.vY = vY;
+      this.config = config;
+      var self = this;
+      window.onkeydown = function (event) {
+        var keyCode = event.keyCode;
+        switch (keyCode) {
+          case 38: // arrow up
+              self.vY -= 1;
+              break;
+            case 40: // arrow down
+              self.vY += 1;
+              //console.log("arrow down pressed");
+              break;
+            case 37: // arrow left
+              self.vX -= 1;
+              // console.log("arrow left pressed");
+              break;
+            case 39: //arrow right
+            self.vX += 1;
+              //console.log("arrow right pressed");
+              break;
+            default:
+            // do nothing
+              break;
+        };
+      };
+  },
+  draw: function() {
+      if (this.stage == null ) {
+          return;
+      }
+      var ctx = this.stage.context;
+      ctx.beginPath();
+      ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+      ctx.fillStyle = this.color;
+      ctx.fill();
+      ctx.stroke();
+  },
+  update: function() {
+      this.x += this.vX;
+      this.y += this.vY;
+      this.radius = this.mass * 0.6;
+  },
+  checkBoundaryCollision: function() {
+      if (this.x - this.radius < 0) {
+          this.x = this.radius;
+          this.vX *= -1;
+      } else if (this.x + this.radius > this.stage.canvas.width) {
+          this.x = this.stage.canvas.width - this.radius;
+          this.vX *= -1;
+      }
+      if (this.y - this.radius < 0) {
+          this.y = this.radius;
+          this.vY *= -1;
+      } else if (this.y + this.radius > this.stage.canvas.height) {
+          this.y = this.stage.canvas.height - this.radius;
+          this.vY *= -1;
+      }
+  },
+  destroy() {
+      this.stage.removeChild(this);
+      delete this;
   }
 }
 
@@ -148,14 +227,16 @@ Config.prototype = {
 window.addEventListener("load", function() {
     var stage = new Stage("canvas","#ccc",60);
     var config = new Config();
+    var player = new Player(200, 200, 15, "#cfa", 0, 0, config);
     for (var i = 0; i < config.enemies; i++) {
       var x = 20 + (Math.random() * (400 ));
       var y = 20 + (Math.random() * (400 ));
-      var mass = 5 + Math.random() * 10;
+      var mass = 5 + Math.random() * 5;
       var vX = Math.random() * 4 - 2;
       var vY = Math.random() * 4 - 2;
       stage.addChild(new Blob(x, y, mass, "#ccc", vX, vY, config));
     }
+    stage.addChild(player);
     stage.rendering();
     }
 );
